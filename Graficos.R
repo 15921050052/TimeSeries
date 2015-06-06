f3214e824a6d9e94d62ed5df35c0f36836f66fd9
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-06-06 11:19:11 +0200

    End ggplot2 ACF, PACF Descompose

--- Graficos.R
+++ Graficos.R
@@ -6,27 +6,25 @@ library(xts)
 library(devtools)
 #install_github('sinhrks/ggfortify')
 library(ggfortify)
-
+#install.packages("gridSVG", repos="http://R-Forge.R-project.org")
+require(gridSVG)
+require(XML)
 
 ACF<-function(serie.ts){
   bacf <- acf(serie.ts, plot = FALSE)
   bacfdf <- with(bacf, data.frame(lag, acf))
   bacfdf$colour <- ifelse((bacfdf$acf < 0.2 | bacfdf$acf < -0.2), "firebrick1",
                        "steelblue")
-  #bacfdf$hjust <- ifelse(bacfdf$acf > 0, 0.2, -0.2)
-#   ggplot(bacfdf, aes(month, value, label = month,
-#                     hjust = hjust)) + geom_text(aes(y = 0,
-#                     colour = colour)) + geom_bar(stat = "identity",
-#                      aes(fill = colour))
+
   if(sum(bacfdf$acf<=0)==0){
     q <- ggplot(data = bacfdf, mapping = aes(x = lag, y = acf)) +
       geom_hline(aes(yintercept = 0)) +
-      geom_segment(mapping = aes(xend = lag, yend = 0,colour=colour))+
+      geom_segment(mapping = aes(xend = lag, yend = 0),col=bpacfdf$colour)+
       geom_line(aes(y = 0.21),col="blue",linetype="dashed")
   }else{
     q <- ggplot(data = bacfdf, mapping = aes(x = lag, y = acf)) +
       geom_hline(aes(yintercept = 0)) +
-      geom_segment(mapping = aes(xend = lag, yend = 0,colour=colour))+
+      geom_segment(mapping = aes(xend = lag, yend = 0),col=bpacfdf$colour)+
       geom_line(aes(y = 0.21),col="blue",linetype="dashed")+
       geom_line(aes(y = -0.21),col="blue",linetype="dashed")
   }
@@ -44,14 +42,14 @@ PACF<-function(serie.ts){
     q <- ggplot(data = bpacfdf, mapping = aes(x = lag, y = acf)) +
       ylab(label = "PACF")+
       geom_hline(aes(yintercept = 0)) +
-      geom_segment(mapping = aes(xend = lag, yend = 0),col=bpacfdf$colour,lineend = "butt")+
+      geom_segment(mapping = aes(xend = lag, yend = 0),col=bpacfdf$colour)+
       geom_line(aes(y = 0.21),col="blue",linetype="dashed")+
       theme(legend.position="none")
   }else{
     q <- ggplot(data = bpacfdf, mapping = aes(x = lag, y = acf)) +
       ylab(label = "PACF")+
       geom_hline(aes(yintercept = 0)) +
-      geom_segment(mapping = aes(xend = lag, yend = 0),col=bpacfdf$colour,lineend = "butt")+
+      geom_segment(mapping = aes(xend = lag, yend = 0),col=bpacfdf$colour)+
       geom_line(aes(y = 0.21),col="blue",linetype="dashed")+
       geom_line(aes(y = -0.21),col="blue",linetype="dashed")+
       theme(legend.position="none")
@@ -73,3 +71,52 @@ DESCOMPOSE<-function(serie.ts){
     facet_grid(variable ~ . , scales = "free")
   
 }
+
+
+
+
+
+
+
+#get the latest version of gridSVG
+
+
+
+#draw a ggplot2 graph
+#thanks http://sharpstatistics.co.uk/r/ggplot2-guide/
+p <- ACF(serie.ts)
+p + facet_grid(. ~ Species) + stat_smooth(method = "lm")
+
+#define a simple html head template
+htmlhead <- 
+  '<!DOCTYPE html>
+<head>
+<meta charset = "utf-8">
+<script src = "http://d3js.org/d3.v3.js"></script>
+</head>
+
+<body>
+'
+
+#use gridSVG to export our plot to SVG
+mysvg <- grid.export("panzoom1.svg")
+
+
+#define a simple pan zoom script using d3
+panzoomScript <-
+  '  <script>
+var svg = d3.selectAll("#gridSVG");
+svg.call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom))
+
+function zoom() {
+svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+} 
+</script>
+</body>
+'
+
+#combine all the pieces into an html file
+sink("panzoom_ggplot2.html")
+cat(htmlhead,saveXML(mysvg$svg),panzoomScript)
+#close our file
+sink(file=NULL)
