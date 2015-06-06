8e869ca06e6b7c10eb78bef2e0b241af11363354
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-06-06 10:43:54 +0200

    Ggplots2 acf pacf

--- Graficos.R
+++ Graficos.R
@@ -4,29 +4,29 @@ library(tseries)
 library(timeSeries)
 library(xts)
 library(devtools)
-install_github('sinhrks/ggfortify')
+#install_github('sinhrks/ggfortify')
 library(ggfortify)
 
 
 ACF<-function(serie.ts){
   bacf <- acf(serie.ts, plot = FALSE)
   bacfdf <- with(bacf, data.frame(lag, acf))
-  bacfdf$colour <- ifelse(bacfdf$acf < 0, "firebrick1",
+  bacfdf$colour <- ifelse((bacfdf$acf < 0.2 | bacfdf$acf < -0.2), "firebrick1",
                        "steelblue")
-  dtm$hjust <- ifelse(dtm$value > 0, 1.3, -0.3)
-  ggplot(dtm, aes(month, value, label = month,
-                    hjust = hjust)) + geom_text(aes(y = 0,
-                    colour = colour)) + geom_bar(stat = "identity",
-                     aes(fill = colour))
+  #bacfdf$hjust <- ifelse(bacfdf$acf > 0, 0.2, -0.2)
+#   ggplot(bacfdf, aes(month, value, label = month,
+#                     hjust = hjust)) + geom_text(aes(y = 0,
+#                     colour = colour)) + geom_bar(stat = "identity",
+#                      aes(fill = colour))
   if(sum(bacfdf$acf<=0)==0){
     q <- ggplot(data = bacfdf, mapping = aes(x = lag, y = acf)) +
       geom_hline(aes(yintercept = 0)) +
-      geom_segment(mapping = aes(xend = lag, yend = 0))+
+      geom_segment(mapping = aes(xend = lag, yend = 0,colour=colour))+
       geom_line(aes(y = 0.21),col="blue",linetype="dashed")
   }else{
     q <- ggplot(data = bacfdf, mapping = aes(x = lag, y = acf)) +
       geom_hline(aes(yintercept = 0)) +
-      geom_segment(mapping = aes(xend = lag, yend = 0))+
+      geom_segment(mapping = aes(xend = lag, yend = 0,colour=colour))+
       geom_line(aes(y = 0.21),col="blue",linetype="dashed")+
       geom_line(aes(y = -0.21),col="blue",linetype="dashed")
   }
@@ -38,19 +38,23 @@ ACF<-function(serie.ts){
 PACF<-function(serie.ts){
   bpacf <- pacf(serie.ts, plot = F)
   bpacfdf <- with(bpacf, data.frame(lag, acf))
+  bpacfdf$colour <- ifelse((bacfdf$acf < 0.2 | bacfdf$acf < -0.2), "red",
+                          "green")
   if(sum(bpacfdf$acf<=0)==0){
     q <- ggplot(data = bpacfdf, mapping = aes(x = lag, y = acf)) +
       ylab(label = "PACF")+
       geom_hline(aes(yintercept = 0)) +
-      geom_segment(mapping = aes(xend = lag, yend = 0))+
-      geom_line(aes(y = 0.21),col="blue",linetype="dashed")
+      geom_segment(mapping = aes(xend = lag, yend = 0),col=bpacfdf$colour,lineend = "butt")+
+      geom_line(aes(y = 0.21),col="blue",linetype="dashed")+
+      theme(legend.position="none")
   }else{
     q <- ggplot(data = bpacfdf, mapping = aes(x = lag, y = acf)) +
       ylab(label = "PACF")+
       geom_hline(aes(yintercept = 0)) +
-      geom_segment(mapping = aes(xend = lag, yend = 0))+
+      geom_segment(mapping = aes(xend = lag, yend = 0),col=bpacfdf$colour,lineend = "butt")+
       geom_line(aes(y = 0.21),col="blue",linetype="dashed")+
-      geom_line(aes(y = -0.21),col="blue",linetype="dashed")
+      geom_line(aes(y = -0.21),col="blue",linetype="dashed")+
+      theme(legend.position="none")
   }
   
   q
