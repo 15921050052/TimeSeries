9065073e0a575ce84a4736b85d2ea2ee8862e299
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-06-06 11:35:36 +0200

    GGplot2 to HTML

--- Graficos.R
+++ Graficos.R
@@ -13,18 +13,18 @@ require(XML)
 ACF<-function(serie.ts){
   bacf <- acf(serie.ts, plot = FALSE)
   bacfdf <- with(bacf, data.frame(lag, acf))
-  bacfdf$colour <- ifelse((bacfdf$acf < 0.2 | bacfdf$acf < -0.2), "firebrick1",
-                       "steelblue")
+  bacfdf$colour <- ifelse((bacfdf$acf < 0.2 | bacfdf$acf < -0.2), "red",
+                       "green")
 
   if(sum(bacfdf$acf<=0)==0){
     q <- ggplot(data = bacfdf, mapping = aes(x = lag, y = acf)) +
       geom_hline(aes(yintercept = 0)) +
-      geom_segment(mapping = aes(xend = lag, yend = 0),col=bpacfdf$colour)+
+      geom_segment(mapping = aes(xend = lag, yend = 0),col=bacfdf$colour)+
       geom_line(aes(y = 0.21),col="blue",linetype="dashed")
   }else{
     q <- ggplot(data = bacfdf, mapping = aes(x = lag, y = acf)) +
       geom_hline(aes(yintercept = 0)) +
-      geom_segment(mapping = aes(xend = lag, yend = 0),col=bpacfdf$colour)+
+      geom_segment(mapping = aes(xend = lag, yend = 0),col=bacfdf$colour)+
       geom_line(aes(y = 0.21),col="blue",linetype="dashed")+
       geom_line(aes(y = -0.21),col="blue",linetype="dashed")
   }
@@ -59,12 +59,13 @@ PACF<-function(serie.ts){
 }
 
 DESCOMPOSE<-function(serie.ts){
-  seriex<-data.frame(Tiempo=1:length(as.vector(as.matrix(decompose(serie.ts)$x))),
+  datos<-data.frame(Tiempo=1:length(as.vector(as.matrix(decompose(serie.ts)$x))),
                      Original=as.vector(as.matrix(decompose(serie.ts)$x)))
-  serietrend<-as.vector(as.matrix(decompose(serie.ts)$trend))
-  serieseasonal<-as.vector(as.matrix(decompose(serie.ts)$seasonal))
-  serierandom<-as.vector(as.matrix(decompose(serie.ts)$random))
-  df<-data.frame(seriex,Trend=serietrend,Seasonal=serieseasonal,Random=serierandom)
+  Tendencia<-as.vector(as.matrix(decompose(serie.ts)$trend))
+  Estacionalidad<-as.vector(as.matrix(decompose(serie.ts)$seasonal))
+  Aleatoriedad<-as.vector(as.matrix(decompose(serie.ts)$random))
+  df<-data.frame(datos,Trend=Tendencia,Seasonal=Estacionalidad,Random=Aleatoriedad)
+  
   dfm<-melt(df,id="Tiempo")
   
   ggplot(na.omit(dfm),aes(x=Tiempo,y=value,colour=variable,group=variable))+geom_line()+
@@ -73,50 +74,39 @@ DESCOMPOSE<-function(serie.ts){
 }
 
 
+HTML<-function(Grafico,Nombre){
+  p<-Grafico
+  
+  
+  htmlhead <-'<!DOCTYPE html>
+      <head>
+      <meta charset = "utf-8">
+      <script src = "http://d3js.org/d3.v3.js"></script>
+      </head>
+      
+      <body>
+      '
+  
+  #use gridSVG to export our plot to SVG
+  mysvg <- grid.export(paste(Nombre,".svg"))
+  
+  
+  #define a simple pan zoom script using d3
+  Script <-'  <script>
+        var svg = d3.selectAll("#gridSVG");
+        svg.call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom))
+        
+        function zoom() {
+        svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+        } 
+        </script>
+        </body>'
+  
+  #combine all the pieces into an html file
+  sink(paste(Nombre,"_ggplot2.html"))
+  cat(htmlhead,saveXML(mysvg$svg),Script)
+  #close our file
+  sink(file=NULL)
+}
 
 
-
-
-
-#get the latest version of gridSVG
-
-
-
-#draw a ggplot2 graph
-#thanks http://sharpstatistics.co.uk/r/ggplot2-guide/
-p <- ACF(serie.ts)
-p + facet_grid(. ~ Species) + stat_smooth(method = "lm")
-
-#define a simple html head template
-htmlhead <- 
-  '<!DOCTYPE html>
-<head>
-<meta charset = "utf-8">
-<script src = "http://d3js.org/d3.v3.js"></script>
-</head>
-
-<body>
-'
-
-#use gridSVG to export our plot to SVG
-mysvg <- grid.export("panzoom1.svg")
-
-
-#define a simple pan zoom script using d3
-panzoomScript <-
-  '  <script>
-var svg = d3.selectAll("#gridSVG");
-svg.call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom))
-
-function zoom() {
-svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-} 
-</script>
-</body>
-'
-
-#combine all the pieces into an html file
-sink("panzoom_ggplot2.html")
-cat(htmlhead,saveXML(mysvg$svg),panzoomScript)
-#close our file
-sink(file=NULL)
